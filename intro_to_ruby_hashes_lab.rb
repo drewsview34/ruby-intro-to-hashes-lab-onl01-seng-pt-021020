9a943b867053cd98944e159385eb6ad32b4a78a3
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-23 08:26:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,13 @@ def monopoly_with_second_tier
   	  :railroads=>
   	  {
   	    :pieces=>4,
+  	    :rent_in_dollars=>
+  	    {
+  	      
+  	    }
   	    :names=>
   	    {
+  	      
   	    }
   	  }
   	}
